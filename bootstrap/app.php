e1170eb9c0e3be88a3d9b19804e843766c20322e
Moayad Tarig <software.moayad@gmail.com>
2025-01-10 11:00:05 +0400

    add global middlware to Force parcing response into Json response

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\ForceJsonResponse;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,7 +13,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'force.json' => ForceJsonResponse::class,
+        ]);
+        $middleware->group('api', [
+            'force.json',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
